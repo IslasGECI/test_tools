e20924f13abe1fe3ce8b9930e6de2609ec3ce57b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-05 20:39:20 +0000

    ♻️  Refactor

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -7,10 +7,14 @@ def test_if_exist_remove():
     write_dummy_file(output_path)
     assert os.path.exists(output_path)
     gtt.if_exist_remove(output_path)
-    assert not (os.path.exists(output_path))
+    assert_not_exist(output_path)
 
 
 def write_dummy_file(output_path):
     f = open(output_path, "w")
     f.write("Woops! I have deleted the content!")
     f.close()
+
+
+def assert_not_exist(output_path):
+    assert not (os.path.exists(output_path))
