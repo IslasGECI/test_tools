2c20c4e73237ed9d349ec58c44bed45c76c44552
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-05 20:39:54 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -5,7 +5,7 @@ import os
 def test_if_exist_remove():
     output_path = "tests/demofile3.txt"
     write_dummy_file(output_path)
-    assert os.path.exists(output_path)
+    assert_not_exist(output_path)
     gtt.if_exist_remove(output_path)
     assert_not_exist(output_path)
 
